1e2fb6655b436d06df5a66c2cb53eb9ec6a4210f
ThuyTQ <tqthuy.ict@gmail.com>
2017-11-15 00:20:46 +0700

    create interface

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Trương Quang Thuỷ on 11/14/17.
+//  Copyright © 2017 ThuyTQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
